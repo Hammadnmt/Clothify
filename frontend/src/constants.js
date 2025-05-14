0be0cbf15d2e5332edecaed8f36b8b3f416a3fe6
Hammad <hammad.alinmt@gmail.com>
2025-05-14 16:25:58 +0500

    live backend URL added

--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -1,8 +1,7 @@
 // export const BASE_URL =
 //   process.env.NODE_ENV === "development" ? "http://localhost:5000" : "";
 
-export const BASE_URL =
-  import.meta.env.VITE_BASE_URL ?? "http://localhost:5000/";
+export const BASE_URL = import.meta.env.VITE_BASE_URL ?? "https://clothify-production.up.railway.app/";
 export const PRODUCTS_URL = "api/v1/products";
 export const CATEGORY_URL = "api/v1/category";
 export const SUB_CATEGORY_URL = "api/v1/category/subcategory";
